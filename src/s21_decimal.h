c62a8c9564808c7aa4d81b82e0b683536785d1bd
whitingb <whitingb@student.21-school.ru>
2025-03-29 15:01:03 +0500

    1

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -12,13 +12,21 @@ typedef union {
     } bits;
 } s21_decimal;
 
-/* arithmetic foo */
+// Arithmetic foo
+/*! @brief Cложение Decimal
+    @param value_1,value_2 - слагаемое
+    @param result - сумма
+    @return код ошибки.
+    0 - ОК
+    1 - число слишком велико или равно бесконечности
+    2 — число слишком мало или равно отрицательной бесконечности; */
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
+
 int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 
-/* comparison foo */
+// comparison foo
 int s21_is_less(s21_decimal, s21_decimal);
 int s21_is_less_or_equal(s21_decimal, s21_decimal);
 int s21_is_greater(s21_decimal, s21_decimal);
@@ -26,13 +34,13 @@ int s21_is_greater_or_equal(s21_decimal, s21_decimal);
 int s21_is_equal(s21_decimal, s21_decimal);
 int s21_is_not_equal(s21_decimal, s21_decimal);
 
-/* transformation foo */
+// transformation foo
 int s21_from_int_to_decimal(int src, s21_decimal *dst);
 int s21_from_float_to_decimal(float src, s21_decimal *dst);
 int s21_from_decimal_to_int(s21_decimal src, int *dst);
 int s21_from_decimal_to_float(s21_decimal src, float *dst);
 
-/* other foo */
+// other foo
 int s21_floor(s21_decimal value, s21_decimal *result);
 int s21_round(s21_decimal value, s21_decimal *result);
 int s21_truncate(s21_decimal value, s21_decimal *result);
